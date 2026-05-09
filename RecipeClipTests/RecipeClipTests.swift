386ed458d07c9b190e136657fc0348706edbbf78
Ono Lee <theonolee@gmail.com>
2026-05-09 23:31:13 +0200

    Initial Commit

---
+++ RecipeClipTests/RecipeClipTests.swift
@@ -0,0 +1,16 @@
+//
+//  RecipeClipTests.swift
+//  RecipeClipTests
+//
+//  Created by Ono Lee on 09.05.26.
+//
+
+import Testing
+
+struct RecipeClipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
